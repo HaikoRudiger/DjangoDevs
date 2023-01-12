3a1d469cf0238d91e2b9a384599919cc5927ddbb
Haipo31 <haikorudiger@gmail.com>
2023-01-12 17:17:12 -0300

    ok

---
+++ projeto venv/alunos/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse ('<h1>Oi sumido rsrs teste 2</h1>')
+
+# Create your views here.
