d8ff73cdfbbf6c7dbfb8eb509b680c0e94d4eaaf
Haiko Rudiger <haiko.rudiger@proway.treina>
2023-01-13 21:44:00 -0300

    ok

--- projeto venv/alunos/views.py
+++ projeto venv/alunos/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def aluno(request):
+    return render(request, 'aluno.html')
+
 # Create your views here.
