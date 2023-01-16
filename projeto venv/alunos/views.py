6f588675b576be18d7add63898efbc77fb115b03
Haipo31 <haikorudiger@gmail.com>
2023-01-16 17:26:38 -0300

    hmtl

--- projeto venv/alunos/views.py
+++ projeto venv/alunos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request,'index.html', {'nome_do_aluno':'Lirinha'})
+
+def aluno(request):
+    return render(request,'aluno.html')
+
 
 # Create your views here.
