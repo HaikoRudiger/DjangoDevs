6900583e5f24cf6b3a3622844fcd2a39a3917b75
Haiko Rudiger <haiko.rudiger@proway.treina>
2023-01-16 22:02:22 -0300

    docker

--- projeto venv/alunos/views.py
+++ projeto venv/alunos/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html', {'nome_do_aluno':'Lirinha'})
+    alunos = {
+       1: 'Lirinha',
+       2: 'Felipe',
+       3: 'Marcos',
+       4: 'Haiko',
+       5: 'Marcio',
+       6: 'Joao'
+       
+   }
+
+    dados = {
+       'nome_do_aluno' : alunos
+   }
+
+    return render(request,'index.html', dados)
 
 def aluno(request):
     return render(request,'aluno.html')
-
-
-def aluno(request):
-    return render(request, 'aluno.html')
 
 # Create your views here.
